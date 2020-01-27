f13ad1257bf01a8d1ba1343241f75f9e175272a4
Rangana-Das <35832416+Rangana-Das@users.noreply.github.com>
2020-01-27 17:27:44 +0530

    Add files via upload

---
+++ formapp2.js
@@ -0,0 +1,26 @@
+const name=document.querySelector('#name');
+const password=document.querySelector('#password');
+const button=document.querySelector('.btn');
+
+loadEventListeners();
+function loadEventListeners(){
+    name.addEventListener('focus', addActive);
+    password.addEventListener('focus', addActive);
+    name.addEventListener('blur', removeActive);
+    password.addEventListener('blur', removeActive);
+    button.addEventListener('click',verifySub);
+    
+}
+function addActive(e){
+    this.parentElement.classList.add('active');
+}
+function removeActive(e){
+    if(this.value==='')
+        this.parentElement.classList.remove('active');
+}
+function verifySub(e){
+    if(name.value===''||password.value==='')
+        alert('Please fill required fields');
+    else
+        window.open("list.html","_blank");
+}
